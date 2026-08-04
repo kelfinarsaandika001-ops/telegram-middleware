9a82dbdcbf652f653df946b8342cc9360befaf7f
kelfinarsaandika001-ops <kelfinarsaandika001@gmail.com>
2026-08-04 13:39:21 +0700

    Update APP_SCRIPT_URL to new endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw-2q3BHyY_sz7xRm1P0i2TPimiZYImaZox7358fE7iG80DxjVNO0chxlL8lm2w5DZL/exec';
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw-hd207gzF4Ctri0XFn_m11PcFOR3AUD7TU6rD5BQiQHX6dft7m22U9C_nox8JTYtV/exec';
 
 app.post('/webhook', async (req, res) => {
   try {
